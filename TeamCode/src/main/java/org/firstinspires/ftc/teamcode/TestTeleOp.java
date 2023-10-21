0935cd8c7e676809e25e8e4dbe04ce296c69e23e
DanielYeum <danielyeum123@gmail.com>
2023-10-21 14:01:16 -0400

    Update TestTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -14,6 +14,8 @@ public class TestTeleOp extends LinearOpMode {
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
 
+        //BRAKE and FLOAT
+        //When setPower(0),
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -24,10 +26,11 @@ public class TestTeleOp extends LinearOpMode {
         leftBack.setTargetPosition(500); //int value is the ticks
 
         //Uses set power to run the motors AND track the position; Run endlessly with constant velocity
+        //Encoder keeps track of the ticks
         leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setPower(0.5);
+        leftBack.setPower(0);
 
-        //
+        //Only runs on the power assigned and doesn't keep track of ticks
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //Completely resets the encoder; used to make sure no encoder is left
@@ -36,13 +39,19 @@ public class TestTeleOp extends LinearOpMode {
 
 
         while(opModeInInit()) {
-
+            telemetry.addData("leftFront power: ", leftFront.getPower());
+            telemetry.update();
         }
 
         waitForStart();
 
         while(opModeIsActive()) {
+            if(gamepad1.a) {
+                leftFront.setPower(0);
+            }
 
+
+            telemetry.update();
         }
     }
 }
