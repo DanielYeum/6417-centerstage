eb2edc6876cb346ddd9019b8a29f5eab7a2ec03d
DanielYeum <danielyeum123@gmail.com>
2023-10-21 14:22:02 -0400

    created test teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+@TeleOp(name = "TestTeleOp",group = "TeleOp")
 public class TestTeleOp extends LinearOpMode {
     DcMotorEx leftFront, leftBack, rightBack, rightFront;
+    double leftVertControl;
+
     @Override
     public void runOpMode() throws InterruptedException {
         //Define the names on the screen to assign motors to the hub
@@ -21,9 +25,14 @@ public class TestTeleOp extends LinearOpMode {
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+
         //Uses ticks to run to the position
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setTargetPosition(500); //int value is the ticks
+        //leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //leftBack.setTargetPosition(500); //int value is the ticks
 
         //Uses set power to run the motors AND track the position; Run endlessly with constant velocity
         //Encoder keeps track of the ticks
@@ -37,7 +46,6 @@ public class TestTeleOp extends LinearOpMode {
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
-
         while(opModeInInit()) {
             telemetry.addData("leftFront power: ", leftFront.getPower());
             telemetry.update();
@@ -46,12 +54,13 @@ public class TestTeleOp extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
+            leftVertControl = -gamepad1.left_stick_y;
             if(gamepad1.a) {
                 leftFront.setPower(0);
             }
+            if(leftVertControl > 0)
 
-
-            telemetry.update();
+                telemetry.update();
         }
     }
 }
